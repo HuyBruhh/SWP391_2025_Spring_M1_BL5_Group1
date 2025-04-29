d7566805017202b6e8a3f7a4d4b0d0f5c7b90990
ducnt14112004 <ducnthe180869@fpt.edu.vn>
2025-04-29 16:21:00 +0700

    Room Fee functions

--- src/java/dao/DBContext.java
+++ src/java/dao/DBContext.java
@@ -15,7 +15,7 @@ public class DBContext {
     public DBContext()
     {
         try{
-            String user="ad";
+            String user="sa";
             String pass="123";
             String url="jdbc:sqlserver://localhost:1433;databaseName=HL_Motel";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
