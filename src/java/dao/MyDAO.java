ae42607b7c1c80b720d3d06c109db05e9987eb8a
hieupt6903 <hieupthe170352@fpt.edu.vn>
2025-05-01 16:57:26 +0700

    fix vnpay payment

--- src/java/dao/MyDAO.java
+++ src/java/dao/MyDAO.java
@@ -7,10 +7,11 @@ import dao.DBContext;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 
 
-public class MyDAO extends DBContext{
+public class MyDAO extends DBContext implements AutoCloseable {
     public Connection con = null;
     public PreparedStatement ps = null;
     public ResultSet rs = null;
@@ -20,12 +21,19 @@ public class MyDAO extends DBContext{
         con = connection;
     }
 
-    public void finalize() {
+    @Override
+    public void close() {
         try {
+            if (rs != null) {
+                rs.close();
+            }
+            if (ps != null) {
+                ps.close();
+            }
             if (con != null) {
                 con.close();
             }
-        } catch (Exception e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
